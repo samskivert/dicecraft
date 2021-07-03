e1ff00fb69b27d4fa619054358ba8636368f6169
Michael Bayne <mdb@samskivert.com>
2021-07-03 09:42:22 -0700

    Give enemies slots too.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : ScriptableObject {
   public new string name;
   public Sprite image;
   public int maxHp;
+  public DamageType[] slots;
   public DieFace[] dice1;
   public DieFace[] dice2;
   public DieFace[] dice3;
