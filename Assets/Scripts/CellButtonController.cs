48d3a8ced9c3423ee3365335dd6b1b93493ff5f3
Michael Bayne <mdb@samskivert.com>
2022-01-15 17:10:06 -0800

    Working on editor.

---
+++ Assets/Scripts/CellButtonController.cs
@@ -0,0 +1,12 @@
+namespace dicecraft {
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellButtonController : MonoBehaviour {
+
+  public Image image;
+  public Toggle toggle;
+  public ScriptableObject cell { get; set; }
+}
+}
---
+++ Assets/Scripts/PaletteController.cs
@@ -0,0 +1,53 @@
+namespace dicecraft {
+
+using System.Linq;
+
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+using React;
+
+public class PaletteController : MonoBehaviour {
+
+  public GameObject labelPrefab;
+  public GameObject cellButtonGridPrefab;
+  public GameObject cellButtonPrefab;
+  public ToggleGroup group;
+
+  public IMutable<ScriptableObject> selectedCell = Values.Mutable<ScriptableObject>(null);
+
+  private void Awake () {
+
+    void AddHeader (string label) {
+      var labelObj = Instantiate(labelPrefab, transform);
+      labelObj.GetComponent<TMP_Text>().text = label;
+      labelObj.SetActive(true);
+    }
+
+    void AddGrid<T> (string label, string query) where T : ScriptableObject {
+      AddHeader(label);
+      var grid = Instantiate(cellButtonGridPrefab, transform);
+      grid.SetActive(true);
+      var guids = AssetDatabase.FindAssets(query);
+      foreach (var path in guids.Select(AssetDatabase.GUIDToAssetPath)) {
+        var data = AssetDatabase.LoadAssetAtPath<T>(path);
+        var cell = (Cell.Info)data;
+        var buttonObj = Instantiate(cellButtonPrefab, grid.transform);
+        var button = buttonObj.GetComponent<CellButtonController>();
+        button.image.sprite = cell.Image;
+        button.cell = data;
+        button.toggle.group = group;
+        button.toggle.onValueChanged.AddListener(sel => {
+          Debug.Log("Toggled " + data + " " + sel);
+          if (sel) selectedCell.Update(data);
+        });
+      }
+    }
+
+    AddGrid<WallData>("Walls", "t:WallData");
+    AddGrid<EnemyData>("Enemies", "t:EnemyData");
+  }
+}
+}
