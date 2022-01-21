e16cae36e761cb7b4777f503cb5d6041f7705971
Michael Bayne <mdb@samskivert.com>
2022-01-21 10:29:56 -0800

    Only trigger if interactable.

--- Assets/Scripts/Popup.cs
+++ Assets/Scripts/Popup.cs
@@ -10,7 +10,7 @@ public class Popup : MonoBehaviour {
 
   private void Update () {
     void Trigger (Button button) {
-      if (button != null) button.onClick.Invoke();
+      if (button != null && button.interactable) button.onClick.Invoke();
     }
     if (Input.GetKeyDown(KeyCode.Return)) Trigger(returnButton);
     else if (Input.GetKeyDown(KeyCode.Escape)) Trigger(escapeButton);
